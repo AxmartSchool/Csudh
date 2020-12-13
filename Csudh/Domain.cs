5f106626104b067595bed9663e80cf6a3048bfc5
Axmart <notaxmart@gmail.com>
2020-12-13 22:26:41 +0100

    Domain class

---
+++ Csudh/Domain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csudh
+{
+    class Domain
+    {
+
+        public string Nev { get; set; }
+        public string IpCim { get; set; }
+
+
+
+
+    }
+}
