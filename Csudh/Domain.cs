65a0172e9a00e83c91f9bfcf9f9b91763832f7d5
Axmart <notaxmart@gmail.com>
2020-12-14 23:34:56 +0100

    Beolvasas

--- Csudh/Domain.cs
+++ Csudh/Domain.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,26 @@ namespace Csudh
         public string IpCim { get; set; }
 
 
+        public static List<Domain> Beolvasas()
+        {
+            var output = new List<Domain>();
+
+            var sr = new StreamReader(@"..\..\csudh.txt", Encoding.UTF8);
+            sr.ReadLine();
+            string[] temptomb;
+            while (!sr.EndOfStream)
+            {
+
+                temptomb = sr.ReadLine().Split(';');
+                output.Add(new Domain() { Nev = temptomb[0], IpCim = temptomb[1] });
+
+            }
+
+
+            return output;
+
+
+        }
 
 
     }
